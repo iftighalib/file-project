b0da19e1945da3223b4607b393659fa5d97a6a6e
iftighalib <113223275+iftighalib@users.noreply.github.com>
2022-09-10 10:32:11 +0700

    pelatihan

---
+++ coba.py
@@ -0,0 +1,44 @@
+# ini fungsi
+def fungsi_python():
+    print("hai ini adalah fungsi")
+
+nama = ("ghalib")
+umur = 18
+tinggibadan = 165.8
+print("nama saya adalah " + nama + " saya berumur" + str(umur))
+print("saya berumur: ", umur)
+print(tinggibadan, "tinggi nya", type(tinggibadan))
+
+orangkaya = True
+pelit = False
+print(orangkaya)
+
+# ayam = 0, kucing =1 or -3, kelinci =2 or -2, tupai =3 or -1
+# hewan = ["ayam", "kucing", 18, "tupai"]
+# hewan.append("bebek")
+# hewan.remove("ayam")
+# print(hewan)
+# hewan[2] = "kelinci"
+# print(hewan)
+# hewanterbang = ("pipit", "elang", "kelinci", "tupai")
+# namamas = {"davin", "kevin"}
+
+# print(hewan[1])
+# print(hewanterbang[1])
+
+angka1 = 5
+angka2 = 10
+print("penjumlahan : " + str(angka1 + angka2))
+
+print("pengurangan : " + str(angka1 - angka2))
+# pembagian yg menunjukan semua hasil
+print("pembagian : " + str(angka1 / angka2))
+# untuk pembagian yg hanya menampilkan bilangan bulat
+print("pembagian : " + str(angka1 // angka2))
+
+print("perkalian : " + str(angka1 * angka2))
+# hasil sisa dari pembagian bilangan bulat
+print("modulus : " + str(angka1 % angka2))
+fungsi_python()
+
+
