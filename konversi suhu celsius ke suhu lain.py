a495d603031dd0d40990e9484a93157b29dee25b
iftighalib <113223275+iftighalib@users.noreply.github.com>
2022-09-16 07:47:58 +0700

    tugas pelkod

---
+++ konversi suhu celsius ke suhu lain.py
@@ -0,0 +1,7 @@
+print("============== konversi suhu celsius ke yang lain ===================")
+
+suhu = float(input("masukan suhu : "))
+print("celsius   = ", suhu, "derajat celsius")
+print("reamur    = ", (suhu*4/5), "derajat reamur")
+print("farenheit = ", (suhu*9/5)+35, "derajat farenheit")
+print("kelvin    = ", suhu+273, "derajat kelvin")
